010c4d9689f1f6116cbb7c449c4eff3f4c0b48cc
mohmmadalabed <mohmmadalabed@gmail.com>
2016-07-22 18:31:13 +0300

    add all files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.payfull.com/opencart22/admin/');
+define('HTTP_CATALOG', 'http://opencart.payfull.com/opencart22/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.payfull.com/opencart22/admin/');
+define('HTTPS_CATALOG', 'http://opencart.payfull.com/opencart22/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/admin/');
+define('DIR_SYSTEM', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/');
+define('DIR_IMAGE', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/image/');
+define('DIR_LANGUAGE', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/admin/language/');
+define('DIR_TEMPLATE', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/admin/view/template/');
+define('DIR_CONFIG', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/config/');
+define('DIR_CACHE', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/storage/cache/');
+define('DIR_DOWNLOAD', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/storage/download/');
+define('DIR_LOGS', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/storage/logs/');
+define('DIR_MODIFICATION', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/storage/modification/');
+define('DIR_UPLOAD', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/system/storage/upload/');
+define('DIR_CATALOG', '/srv/users/serverpilot/apps/payfull-opencart/public/opencart22/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', '2ebb13f07dcb');
+define('DB_PASSWORD', 'ff2da526ff71baaa');
+define('DB_DATABASE', 'cms_opencart22');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
