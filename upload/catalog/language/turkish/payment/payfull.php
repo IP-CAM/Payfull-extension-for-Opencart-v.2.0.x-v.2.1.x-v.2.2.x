837d53f01c20a1a0e045a29dab35f7b9b5b7db47
mohmmadalabed <mohmmadalabed@gmail.com>
2016-08-22 18:00:06 +0300

    add turkish language

--- upload/catalog/language/turkish/payment/payfull.php
+++ upload/catalog/language/turkish/payment/payfull.php
@@ -1,34 +1,35 @@
 <?php
 
 // Text
-$_['text_payfull']      = 'Payfull Payment Gateway';
+$_['text_payfull']      = 'Kredi Kartı İle Öde (PAYFULL)';
 $_['text_title']        = 'Payfull Payment Gateway';
-$_['text_3d']           = 'Use 3D secure Payments System';
-$_['text_installments'] = 'Installments';
-$_['text_credit_card']  = 'Credit card/Debit card';
-$_['text_wait']         = 'wait';
-$_['text_invalid_card'] = 'Invalid card';
-$_['text_loading']      = 'Loading';
+$_['text_3d']           = '3D Secure Kullanın';
+$_['text_installments'] = 'Taksit';
+$_['text_credit_card']  = 'Kredi Kartı/Banka Kartı';
+$_['text_wait']         = 'Bekleyiniz';
+$_['text_invalid_card'] = 'Hatalı Kart Numarası';
+$_['text_loading']      = 'Bekleyiniz';
 
-$_['button_confirm']    = 'Confirm';
+$_['button_confirm']    = 'Ödemeyi Tamamla';
 
-$_['entry_cc_month']     = 'Month';
-$_['entry_cc_year']      = 'Year';
-$_['entry_cc_name']      = 'Name on Card';
-$_['entry_cc_number']    = 'Credit Card Number';
-$_['entry_cc_date']      = 'Expiration Date';
-$_['entry_cc_cvc']       = 'Card Verification Number';
+$_['entry_cc_month']     = 'Ay';
+$_['entry_cc_year']      = 'Yıl';
+$_['entry_cc_name']      = 'Kart Üzerindeki İsim';
+$_['entry_cc_number']    = 'Kart Numarası';
+$_['entry_cc_date']      = 'Son Kullanma Tarihi';
+$_['entry_cc_cvc']       = 'Güvenlik Kodu';
 
-$_['entry_field_required']      = 'field is required';
-$_['entry_field_is_not_number'] = 'field is not numeric';
-$_['entry_field_is_long']       = 'field has long value';
-$_['entry_cc_not_supported']    = 'Please enter a valid credit card number.';
-$_['entry_cc_cvc_wrong']        = 'Please enter a valid credit card verification number.';
-$_['entry_cc_date_wrong']       = 'Incorrect credit card expiration date.';
-$_['entry_3d_not_available']    = '3D Secure Is Not Available';
+$_['entry_field_required']      = 'Bu Alanı Doldurmalısınız';
+$_['entry_field_is_not_number'] = 'Buraya Sayısal Bir Değer Girmeyiniz';
+$_['entry_field_is_long']       = 'Lütfen Daha Kısa Bir İçerik Giriniz';
+$_['entry_cc_not_supported']    = 'Lütfen Geçirli Kart Numarası Giriniz.';
+$_['entry_cc_cvc_wrong']        = 'Lütfen Geçirli Bir Güvenlik Numarası Giriniz';
+$_['entry_cc_date_wrong']       = 'Kartınızın Son Kullanma Tarihini Hatalı Girdiniz';
+$_['entry_3d_not_available']    = '3D Secure Aktif Değildir';
 
-$_['entry_payfull_installmet']  = 'Installment';
-$_['entry_payfull_amount']      = 'Amount / Month';
-$_['entry_payfull_total']       = 'Total';
+$_['entry_payfull_installmet']  = 'Taksit';
+$_['entry_payfull_amount']      = 'Tutar / Ay';
+$_['entry_payfull_total']       = 'Toplam Tutar';
 
-$_['commission_sub_total_title'] = 'Inst Commission';
+$_['commission_sub_total_title'] = 'Taksit Komisyonu';
+
