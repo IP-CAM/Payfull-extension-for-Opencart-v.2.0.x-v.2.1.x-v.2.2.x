2a02b2781b5a210e7d4fb23be8692c3ea86dc465
mohmmadalabed <mohmmadalabed@gmail.com>
2016-08-22 17:24:48 +0300

    add turkish language

---
+++ upload/catalog/language/turkish/payment/payfull.php
@@ -0,0 +1,34 @@
+<?php
+
+// Text
+$_['text_payfull']      = 'Payfull Payment Gateway';
+$_['text_title']        = 'Payfull Payment Gateway';
+$_['text_3d']           = 'Use 3D secure Payments System';
+$_['text_installments'] = 'Installments';
+$_['text_credit_card']  = 'Credit card/Debit card';
+$_['text_wait']         = 'wait';
+$_['text_invalid_card'] = 'Invalid card';
+$_['text_loading']      = 'Loading';
+
+$_['button_confirm']    = 'Confirm';
+
+$_['entry_cc_month']     = 'Month';
+$_['entry_cc_year']      = 'Year';
+$_['entry_cc_name']      = 'Name on Card';
+$_['entry_cc_number']    = 'Credit Card Number';
+$_['entry_cc_date']      = 'Expiration Date';
+$_['entry_cc_cvc']       = 'Card Verification Number';
+
+$_['entry_field_required']      = 'field is required';
+$_['entry_field_is_not_number'] = 'field is not numeric';
+$_['entry_field_is_long']       = 'field has long value';
+$_['entry_cc_not_supported']    = 'Please enter a valid credit card number.';
+$_['entry_cc_cvc_wrong']        = 'Please enter a valid credit card verification number.';
+$_['entry_cc_date_wrong']       = 'Incorrect credit card expiration date.';
+$_['entry_3d_not_available']    = '3D Secure Is Not Available';
+
+$_['entry_payfull_installmet']  = 'Installment';
+$_['entry_payfull_amount']      = 'Amount / Month';
+$_['entry_payfull_total']       = 'Total';
+
+$_['commission_sub_total_title'] = 'Inst Commission';
